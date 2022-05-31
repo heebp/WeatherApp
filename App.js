fd39a2560e9d3889c907d681661dd0dccd3e43c4
qqOwOqq <skdnfl4343@gmail.com>
2022-05-31 11:24:26 +0900

    Context API 추가

--- App.js
+++ App.js
@@ -5,30 +5,36 @@
  * @format
  * @flow strict-local
  */
-import WeatherHomeScreen from './src/WeatherHomeScreen'
+import WeatherHomeScreen from './src/WeatherHomeScreen/WeatherHomeScreen'
 import CategoryScreen from './src/CategoryScreen'
-import WeatherDetailScreen from './src/WeatherDetailScreen'
-import ClothesSheetScreen from './src/ClothesSheetScreen';
-import React from 'react';
+import WeatherDetailScreen from './src/WeatherDetailScreen/WeatherDetailScreen'
+import ClothesSheetScreen from './src/ClothesSheetScreen/ClothesSheetScreen';
+import LocationScreen from './src/LocationScreen/LocationScreen';
+import React, { useContext, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LocationScreen from './src/LocationScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { CurrentLocation } from './src/Context/CurrentLocation';
+import { CurrentWeather } from './src/Context/CurrentWeather';
 const Stack = createNativeStackNavigator();
 
 function App() {
+
   return (
     <SafeAreaProvider>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="WeatherHome" component={WeatherHomeScreen} options={{title:'현재 날씨'}}/>
-        <Stack.Screen name="Category" component={CategoryScreen} />
-        <Stack.Screen name="WeatherDetail" component={WeatherDetailScreen} options={{title:'상세 날씨'}} />
-        <Stack.Screen name="ClothesSheet" component={ClothesSheetScreen} options={{title:'의상 기록'}} />
-        <Stack.Screen name="Location" component={LocationScreen} options={{title:'지역 검색'}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <CurrentLocation>
+        <CurrentWeather> 
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="WeatherHome" component={WeatherHomeScreen} options={{title:'현재 날씨'}}/>
+              <Stack.Screen name="Category" component={CategoryScreen} />
+              <Stack.Screen name="WeatherDetail" component={WeatherDetailScreen} options={{title:'상세 날씨'}} />
+              <Stack.Screen name="ClothesSheet" component={ClothesSheetScreen} options={{title:'의상 기록'}} />
+              <Stack.Screen name="Location" component={LocationScreen} options={{title:'위치 설정'}} />
+            </Stack.Navigator>
+          </NavigationContainer>
+         </CurrentWeather> 
+      </CurrentLocation>
     </SafeAreaProvider>
   );
 }
