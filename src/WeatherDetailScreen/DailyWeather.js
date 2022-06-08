a67d1fc8a7c93f567281dfe81d29a9e069d7b82a
qqOwOqq <skdnfl4343@gmail.com>
2022-06-08 15:21:29 +0900

    아이콘 추가

--- src/WeatherDetailScreen/DailyWeather.js
+++ src/WeatherDetailScreen/DailyWeather.js
@@ -9,12 +9,12 @@ import {
 import { useEffect } from 'react';
 import {API_KEY} from '@env'
 import { LocationContext } from '../Context/CurrentLocation';
+import WeatherIcon from './DetailScreenWeatherIcon';
 const Stack = createNativeStackNavigator();
 
 function DailyWeather(props) {
 const location = useContext(LocationContext);
 const [days, setDays] = useState([]);
-
 const getForecastWeather = async (lat, lon) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,current,minutely,hourly,&appid=${API_KEY}&units=metric`
@@ -22,7 +22,15 @@ const getForecastWeather = async (lat, lon) => {
   const json = await response.json();
   //console.log(json.daily)
   setDays(json.daily);
+
+      
 };
+function getTodayLabel(time){
+  var week = new Array('일','월','화','수','목','금','토');
+  var today = new Date(time).getDay();
+  var todayLabel = week[today];
+  return todayLabel;
+}
   useEffect(() => {
     console.log("데일리 웨더 리렌더링")
     getForecastWeather(location.lat, location.lng);
@@ -42,11 +50,14 @@ const getForecastWeather = async (lat, lon) => {
             <View style={styles.days}>{
            days.slice(0,7).map((day, index) => (
              <View key={index} style={styles.day}>
-               <Text style={styles.tinytext}>{new Date(day.dt * 1000).toString().substring(0, 3)}</Text> 
-               <Text style={styles.description}>{day.weather[0].main}</Text>
-               <Text style={styles.description}>{parseInt(day.temp.max)}</Text>
-               <Text style={styles.description}>{parseInt(day.temp.min)}</Text>
-               <Text style={styles.description}>{day.rain == undefined ? "0mm":Math.round(day.rain*100)+"mm"}</Text>
+                <Text style={styles.tinytext}>{getTodayLabel(day.dt*1000)}</Text> 
+                  <WeatherIcon value={day.weather[0].main}/>
+                <Text style={styles.description}>
+                  {day.weather[0].main}
+                </Text>
+                <Text style={styles.high}>{parseInt(day.temp.max)}도</Text>
+                <Text style={styles.low}>{parseInt(day.temp.min)}도</Text>
+                <Text style={styles.description}>{day.rain == undefined ? "0mm":Math.round(day.rain*100)+"mm"}</Text>
              </View>
            ))
         }
@@ -60,10 +71,16 @@ const getForecastWeather = async (lat, lon) => {
             flexDirection: "row",
         },
         day:{
-          borderWidth: 1,
+          width:50
         },
         tinyText: {
           fontSize: 1,
         },
+        high:{
+          color:"red"
+        },
+        low:{
+          color:"blue"
+        }
     })
     export default DailyWeather
--- src/WeatherDetailScreen/HourlyWeather.js
+++ src/WeatherDetailScreen/HourlyWeather.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext} from 'react';
+import React, { useState, useContext,} from 'react';
 import {
   StyleSheet,
   Text,
@@ -8,24 +8,32 @@ import {
 import { useEffect } from 'react';
 import {API_KEY} from '@env'
 import { LocationContext } from '../Context/CurrentLocation';
+import { NavigationContainer, useNavigation,useIsFocused } from '@react-navigation/native';
+import 'moment/locale/ko'
+import WeatherIcon from './DetailScreenWeatherIcon';
+
+//moment.locale('ko')
+
 
 function HourlyWeather() {
-
+const isFocused = useIsFocused()
 const [hours, setHours] = useState([]);
 const location = useContext(LocationContext);
-//마운트 될때만 렌더링 => useState dt값 수정 필요
+
 const getForecastWeather = async (lat, lon) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,current,minutely,daily&appid=${API_KEY}&units=metric`
   );
   const json = await response.json();
+  //console.log("json확인",json.hourly)
   setHours(json.hourly);
 };
- 
+
   useEffect(() => {
-    console.log("아월리 웨더 리렌더링")
+
     getForecastWeather(location.lat, location.lng);
-  }, []);
+    
+  }, [isFocused]);
       return (
         <View style={styles.screen}>
           {hours.length === 0 ? (
@@ -38,33 +46,44 @@ const getForecastWeather = async (lat, lon) => {
           </View>
         ) : (
           <View style={styles.hours}>{
-          hours.slice(0,7).map((hour, index) => (
+          hours.slice(0,12).map((hour, index) => (
             <View key={index} style={styles.hour}>
-              <Text style={styles.tinytext}>{new Date(hour.dt * 1000).toString().substring(16, 18)}</Text> 
-              <Text style={styles.tinytext}>{hour.weather[0].main}</Text>
-              <Text style={styles.tinytext}>{parseInt(hour.temp)}</Text>
+              <Text style={styles.tinytext}>{new Date(hour.dt * 1000).toString().substring(16, 18)}시</Text> 
+                <WeatherIcon value={hour.weather[0].main}/>
+{/*                 
+              <Text style={styles.tinytext}>
+                {hour.weather[0].main}
+              </Text>
+               */}
+              <Text style={styles.tinytext}>{parseInt(hour.temp)}도</Text>
               {/* <Text style={styles.tinytext}>{hour.weather[0].description}</Text> */}
-              <Text style={styles.tinytext}>{hour.rain == undefined ? '0mm' : Math.round(hour.rain*100)+"mm"}</Text>
+              <Text style={styles.tinytext}>{hour.rain == undefined ? '0mm' : Math.round(hour.rain['1h']*100)+"mm"}</Text>
             </View>
-
           ))
           }
           </View>
         )}
-        
         </View>
       );
     }
     const styles = StyleSheet.create({
+      screen:{
+        justifyContent:"center"
+      },
+
+      bandtext:{
+        color:"white"
+      },
       hours:{
         flexDirection:"row"
       },
-        hour:{
-          flexDirection: "column",
-          borderWidth: 1,
-        },
-        tinyText: {
-          fontSize: 1,
-        },
+      hour:{
+        width:70
+      },
+      tinytext: {
+        color:"white",
+        margin:2,
+        fontSize: 15,
+      },
     })
     export default HourlyWeather
