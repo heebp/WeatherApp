7d8122cd3b429b66e3cc9b4e5a5697f0486589d9
qqOwOqq <skdnfl4343@gmail.com>
2022-06-08 15:21:15 +0900

    scrollview 추가

--- src/WeatherDetailScreen/WeatherDetailScreen.js
+++ src/WeatherDetailScreen/WeatherDetailScreen.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component,useRef, useState} from 'react';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
@@ -10,33 +10,39 @@ import {
   Alert,
   TouchableOpacity,
   Image,
+  ScrollView,
+  Dimensions,
 } from 'react-native';
 import { useLayoutEffect,useEffect } from 'react';
 import HourlyWeather from './HourlyWeather';
 import DailyWeather from './DailyWeather';
-
+const fullWidth = Dimensions.get('window').width
+const fullHeight = Dimensions.get('window').height
 
 function WeatherDetailScreen({navigation}) {
+    const scrollviewRef = useRef()
+
     console.log("웨더 디테일 렌더 로드")
-    useLayoutEffect(()=>{
-        navigation.setOptions({
-          headerLeft: () => (
-            <TouchableOpacity
-            title="Category"
-            onPress={ () => navigation.navigate('Category')}>
-              <Image source={ require('../images/categoryButton.png') } style={ { width: 30, height: 30, } } />
-            </TouchableOpacity>
-          ),
-        })
-      })
+    useEffect(()=>{
+
+    },[navigation])
       return (
         <View style={styles.screen}>
-          <View style={styles.top}>
-            <Text>시간별 날씨</Text>
-            <HourlyWeather style={styles.screen}/>
+          <View style={styles.band}>
+
+          </View>
+          <View style={styles.band2}>
+            <Text style={styles.bandtext}>시간별 날씨</Text>
+          </View>
+          <View style={styles.hourly}>
+            <ScrollView pagingEnabled horizontal showsHorizontalScrollIndicator={false} ref={scrollviewRef}>
+              <HourlyWeather style={styles.screen}/>
+            </ScrollView>
+          </View>
+          <View  style={styles.band2}>
+            <Text style={styles.bandtext}>주간날씨</Text>
           </View>
           <View style={styles.bottom}>
-            <Text>주간별 날씨</Text>
             <DailyWeather style={styles.screen}/>
           </View>
         </View>
@@ -44,24 +50,45 @@ function WeatherDetailScreen({navigation}) {
     }
     const styles = StyleSheet.create({
         screen:{
-          flex: 1,
+          flex: 2,
           alignItems: 'center', 
           justifyContent: 'center' 
+        },
+        band:{
+          flex:0.4,
+          width: Dimensions.get('window').width,
+          alignItems: 'flex-start', 
+          justifyContent: 'center' ,
+          backgroundColor:"darkslateblue"
+        },
+        band2:{
+          flex:0.05,
+          width: Dimensions.get('window').width,
+          alignItems: 'flex-start', 
+          justifyContent: 'center' ,
+          backgroundColor:"black"
+        },
+        bandtext:{
+          color:"white",
+          fontSize:18,
         },
         tinyText: {
           fontSize: 1,
         },
-        top: {
-          flex: 0.5,
-          backgroundColor: "beige",
-          borderWidth: 5,
+        hourly: {
+          flex: 0.2,
+          backgroundColor: "darkslateblue",
+
+          justifyContent:"center",
+          alignItems: 'center',
+          alignContent:"center",
+          
         },
         bottom: {
           flex: 0.5,
+          width: Dimensions.get('window').width,
           backgroundColor: "pink",
-          borderWidth: 5,
-          borderBottomLeftRadius: 20,
-          borderBottomRightRadius: 20,
+
         },
     })
     export default WeatherDetailScreen
