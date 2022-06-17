035ccbe403b4daa12fdc4146c4e2909d60f16c76
qqOwOqq <skdnfl4343@gmail.com>
2022-06-17 20:01:34 +0900

    의상 기록 이미지 db 연동

--- src/ClothesSheetScreen/ClothesSheetScreen.js
+++ src/ClothesSheetScreen/ClothesSheetScreen.js
@@ -8,54 +8,34 @@ import {
   View,
   Button,
   Alert,
-  TouchableOpacity,
-  Image,
-  FlatList,
-  ScrollView
+  Dimensions,
 } from 'react-native';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import { WeatherContext } from '../Context/CurrentWeather';
 import moment from 'moment';
 import { DBContext } from '../Context/DataBase';
 import ClothesTag from './ClothesTag';
-import WeatherIcon from '../WeatherIcon';
+import WeatherIcon from '../assets/WeatherIcon';
 import ClothesImage from './ClothesImage';
-const Stack = createNativeStackNavigator();
 
 function ClothesSheetScreen({route, navigation}) {
-  const isFocused = useIsFocused();
   const db = useContext(DBContext)
   const {item} = route.params
-  console.log(item)
   const weather = useContext(WeatherContext);
   const nowTime = moment().format('YYYY/MM/DD');
   const [memo, setMemo] = useState('오늘의 메모');
   const [image, setImage] = useState(null); // 이미지 삽입
   const [tags, setTags] = useState() 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-    console.log(result);
-    if (!result.cancelled) {
-      setImage(result.uri);
-    }
-  };
+  
 
   const saveData = () => {
-    console.log("db test")
     try{
       //의상 기록 저장
       db.transaction((tx) => {
         tx.executeSql(`INSERT INTO clothes_sheet(date,weather,temperature,windchill,memo) VALUES(?,?,?,?,?);`,
         [nowTime.toString().substring(0,10),weather.currentWeather,Math.round(weather.temp),Math.round(weather.windchill),memo],
-        //INSERT INTO clothes_sheet_image(date,image) VALUES(`+nowTime.toString().substring(0,16)+`,`+image+`);
           (tx, results) => {
           inserClothesTag()
+          insertImage()
           alert('저장되었습니다.')
           console.log("저장 로그->")
           console.log(results)
@@ -87,8 +67,9 @@ function ClothesSheetScreen({route, navigation}) {
         tx.executeSql(`UPDATE clothes_sheet SET memo ='`+memo+`' where date='`+item.date+`';`,
         [],
           (tx, results) => {
-
             inserClothesTag()
+            insertImage()
+
           alert('저장되었습니다.')
         },
         (error)=>{
@@ -120,9 +101,49 @@ function ClothesSheetScreen({route, navigation}) {
       }
     }  
   }
+  const insertImage=()=>{
+    var date 
+    if(item.date == null || item.date == undefined){
+      date = nowTime
+    }else{
+      date = item.date
+    }
+    deleteImage()
+    //clothes_tag에 추가
+    for(let j = image.length-1; j>-1; j-- ){
+      try{
+        db.transaction((tx) => {
+          tx.executeSql(`INSERT INTO clothes_sheet_image(date,image) VALUES(?,?)`,
+          [date,image[j]],
+            (tx, results) => {
+            },
+          (error)=>{
+            console.log('에러발생',error);
+          });
+        });
+      }catch(error){
+        console.log(error);
+      }
+    }  
+  }
+  const deleteImage=()=>{
+    try{
+      db.transaction((tx) => {
+        tx.executeSql(`DELETE from clothes_sheet_image WHERE clothes_sheet_image.date ='`+item.date+`';`,
+        [],
+          (tx, results) => {
+
+        },
+        (error)=>{
+          console.log('에러발생',error);
+        });
+      });
+    }catch(error){
+      console.log(error);
+    }
+  }
   //sheet_tag 추가
   const insertSheetTag=(tagNum)=>{
-    console.log("insertSheetTag")
     var date 
     if(item.date == null || item.date == undefined){
       date = nowTime
@@ -153,7 +174,6 @@ function ClothesSheetScreen({route, navigation}) {
         IN( SELECT sheet_tag.date from sheet_tag LEFT JOIN clothes_tag on sheet_tag.tagNum = clothes_tag.tagNum WHERE sheet_tag.date='`+item.date+`' );`,
         [],
           (tx, results) => {
-            //console.log(tags.length)
 
         },
         (error)=>{
@@ -164,41 +184,38 @@ function ClothesSheetScreen({route, navigation}) {
       console.log(error);
     }
   }
-
-  //하위 컴포넌트 값 
-  const highfunction= (clothesTags) =>{
-     console.log("상위")
-     console.log(clothesTags);
-     
+  const tagHighfunction= (clothesTags) =>{
     setTags(clothesTags);
   }
 
+  const imageHighfunction= (imageSource) =>{
+    setImage(imageSource)
+ }
   useEffect(() => {
     //저장된 db가 있다면 -< db memo을 initialize
     if(item!=-1){
       setMemo(item.memo)
     }
-
   }, [item]);
 
   return(
-    <View style={{ flex: 1}}>
-      <View style={{ width:"90%", backgroundColor:"lightgray",margin:10}}>
+    <View style={styles.screen}>
+      <View style={styles.sheethead}>
       {item === -1 ? (
-        <View style={{ alignSelf:"flex-start", margin:10}}>
+        <View style={styles.sheet}>
           <Text>날짜 : {nowTime} </Text>
           <Text>날씨 : 
-            <WeatherIcon value={weather.currentWeather} style={{}}/>
+            <WeatherIcon value={weather.currentWeather}/>
             {/*weather.currentWeather*/}
           </Text>
           <Text>온도 : {Math.round(weather.temp)}도</Text>
           <Text>체감온도 :{Math.round(weather.windchill)}도</Text>
         </View>
       ):(
-        <View style={{ alignSelf:"flex-start", margin:10}}>
+        <View style={styles.sheet}>
           <Text>날짜 : {item.date} </Text>
           <Text>날씨 : 
-            <WeatherIcon value={item.weather}/>
+            <WeatherIcon value={item.weather} size={20}/>
             {/*item.weather*/}
           </Text>
           <Text>온도 : {item.temperature}도</Text>
@@ -207,15 +224,13 @@ function ClothesSheetScreen({route, navigation}) {
         )
       }
       </View>
-      <View style={{ width:"90%",height:"40%", backgroundColor:"lightgray",margin:10}}>
-        <ClothesTag value={item} propfunction={highfunction}/>
+      <View style={styles.tagcontainer}>
+        <ClothesTag value={item} propfunction={tagHighfunction}/>
       </View>
-      <View>
-        <ClothesImage/>
+      <View style={styles.imagecontainer}>
+        <ClothesImage value={item} propfunction={imageHighfunction}/>
       </View>
-    {/*     
-    <Button title="사진 불러오기" onPress={pickImage} />
-    {image && <Image source={{ uri: image }} style={{ width: 150, height: 150 }} />} */}
+    
     <View>
       <TextInput
         style={styles.input}
@@ -234,16 +249,41 @@ function ClothesSheetScreen({route, navigation}) {
   )
 }
 const styles = StyleSheet.create({
-  input: {
-    borderWidth: 1,
+  screen:{
+    flex: 1,
+    alignItems:'center'
+  },
+  sheethead:{
+    width:"90%",
+    backgroundColor:"lightgray",
+    borderRadius:20,
+    margin:10,
+    elevation: 8,
+  },
+  sheet:{
+    alignSelf:"flex-start",
+    margin:10
+  },
+
+  tagcontainer:{
+    width:"90%",
+    height:"40%", 
+    backgroundColor:"lightgray",
+    margin:10,
+    borderRadius:20,
+    elevation: 8,
   },
   tag:{
 
   },
-  screen:{
-    flex: 1,
-    alignItems: 'center', 
-    justifyContent: 'center' 
+  imagecontainer:{
+    width:380
+  },
+  input: {
+    borderWidth: 1,
+    width:380,
+    marginTop:20,
+    
   },
 })
 
